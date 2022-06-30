0e19c5f14d8979ce522666253a6450e7cf0623b2
Laís Nascimento Baltar Simões <lah.baltar@gmail.com>
2022-06-30 16:41:30 -0300

    Arquivo inicial WPF com Caliburn funcionando de acordo com o passo a passo

---
+++ App/Wpf.Aplicativo/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using Caliburn.Micro;
+using Wpf.Aplicativo.ViewModels;
+
+namespace Wpf.Aplicativo
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            base.Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Define qual será o Controlador padrão
+            // Nota: O controlador MainViewModel ainda não foi criado nessa etapa.
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ App/Wpf.Aplicativo/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Caliburn.Micro;
+using Wpf.Aplicativo.ViewModels;
+
+namespace Wpf.Aplicativo.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+
+        private string nomeCompleto;
+
+        public string NomeCompleto
+        {
+            get
+            {
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+                // Observa se o atributo/instância FirstName recebeu alguma atualização.
+                base.NotifyOfPropertyChange(() => NomeCompleto);
+            }
+        }
+    }
+}
